353da17f5f6a00764deddc1f38f4e1ce8048693f
Mikl1388 <mikl1388@outlook.com>
2022-10-31 04:29:03 +0700

    Create

---
+++ index.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/donate")
+def donate():
+    return render_template("donate.html")
+
+
+@app.route("/posts", methods=["POST", "GET"])
+def posts():
+    if request.method == "POST":
+        searchRequest = request.form['searchRequest']
+        return searchRequest
+    else:
+        return "homo"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
