56f91c3527da960602768667836951b7e2c382df
MrTOOGLE <Leva.mokrov@gmail.com>
2022-11-03 00:02:22 +0700

    backend 1
    + test

---
+++ back_end.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+def registration(cursor, name, password):
+    cursor.execute()
+
+
+def main():
+    connection = sqlite3.connect("test.db")
+    cursor = connection.cursor()
+    registration(cursor, "Alex", "1234")
+
+
+if __name__ == "__main__":
+    main()
