538b8a91d87832b4f699fa6abdd156d794e6cd12
larongg <76695780+larongg@users.noreply.github.com>
2022-11-04 10:00:39 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send
-
+import back_end as be
 
 app = Flask(__name__)
 app.debug = True
-app.config['SECRET'] = "secret!123"
-socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route("/")
@@ -26,23 +23,19 @@ def add_post():
 @app.route("/posts", methods=["POST", "GET"])
 def posts():
     if request.method == "POST":
-        search_request = request.form['searchRequest']
-        return search_request
+        searchRequest = request.form['searchRequest']
+        return searchRequest
     else:
         return "homo"
 
+@app.route("/login", methods=["POST"])
+def log_in():
+    if request.method == "POST":
+        login = request.form['login']
+        password = request.form['password']
+        be.login(login, password)
 
-@socketio.on('message')
-def handle_message(message):
-    print(message)
-    if message != "User connected!":
-        send(message, broadcast=True)
-
-
-@app.route("/online-chat")
-def online_chat():
-    return render_template("online-chat.html")
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="localhost", allow_unsafe_werkzeug=True)
+    app.run(debug=True)
